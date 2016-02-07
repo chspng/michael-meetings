bdb3e9f67e5b82086f31f4e35158c96e591f5efa
Christine P'ng <chspng@gmail.com>
2016-02-07 16:52:57 -0500

    Initial commit: class list and script comments.

---
+++ michael-meetings.py
@@ -0,0 +1,23 @@
+# michael-meetings.py
+
+# input: 
+#	list of names as text file
+#	x number of output configurations
+
+# output: 
+#	x lists of names re-ordered each time
+
+# notes:
+#	if pairs of people should kept together, list them as john & jane in the text file input 
+#	(to be modeled as a single person)
+
+# algorithm:
+#	1. order names (can be random or not)
+#	2. randomly select starting point
+#	3. Based on x, determine what step-size y to iterate by
+#	4. Generate x lists, step through start-point by y
+#	5. Randomize order of x lists
+
+
+
+# test cases
