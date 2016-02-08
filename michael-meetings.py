a53d55c6f75c44fa3f1d3258e002b9d6d9472a85
Christine P'ng <chspng@gmail.com>
2016-02-08 14:06:59 -0500

    - Adding regenerated class lists - Updated class list to remove Coline and put pairs on separate lines (beginning at even-numbered start-points) - Updated script to not scramble name list and add 'extra' iterations at the end - Edited script algorithm notes accordingly

--- michael-meetings.py
+++ michael-meetings.py
@@ -8,19 +8,11 @@
 # output: 
 #	x lists of names re-ordered each time in separate text files
 
-# notes:
-#	if pairs of people should kept together, list them in the same line in the text file input 
-#	(to be modeled as a single person)
-
 # algorithm:
-#	1. randomize names
-#	2. Based on x, determine what step-size y to iterate by
+#	1. list names in desired order (because it's an even number, pairs should be placed at even start-points)
+#	2. Based on x, determine what step-size y to iterate by (in this case 2) -- add extra lists as necessary
 #	3. Generate x lists, step through start-point by y
 #	4. Randomize order of x lists
-
-# to do:
-#	1. format output to separate pairs onto their own lines
-# 	2. ensure that pairs are distant and everyone has spot in every "slot" if possible (can use 'extra' iterations for this)
 
 #!/bin/python3
 import random
@@ -32,27 +24,25 @@ def michaelmeetings( classlist, numberoflists ):
 		names = f.readlines()
 		
 	names = [x.strip('\n') for x in names]
-	random.shuffle(names)
-
-	# calculate step size (
-	### this could be thrown off if there are many pairs
+	
+	# calculate step size
 	stepsize = round(len(names)/numberoflists)
 	startpoints = list(range(0, len(names), stepsize))
 	
+	random.shuffle(startpoints)
+
 	# if there are fewer startpoints selected than numberoflists, add additional ones
-	### this fix is sloppy -- wouldn't work well in more extreme cases
 	if (len(startpoints) < numberoflists):
 		missingcount = numberoflists - len(startpoints)
 		while (missingcount > 0):
-			additionalstart = 1 # chose this because the first start is 0, so if stepsize > 1, this is a unique start
+			# randomly select even number
+			additionalstart = random.randrange(0, len(startpoints) + 1, 2) 
 			missingcount -= 1
 			startpoints.append(additionalstart)
 			additionalstart = additionalstart + stepsize
-	
-	random.shuffle(startpoints)
 
 	# generate ordered lists & output
-	weekcount = 1 # the number of startpoints should equal the numberoflists
+	weekcount = 1 
 
 	for start in startpoints:
 		filename = "week" + str(weekcount) + "-name-list.txt"
